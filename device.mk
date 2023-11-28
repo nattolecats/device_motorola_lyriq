c243cd5fc27dc1799c62059aeeb795a440b454f8
lahaina <nattolecats@gmail.com>
2023-11-28 21:34:33 +0900

    lyriq: Switch to WiFi service AIDL

--- device.mk
+++ device.mk
@@ -336,7 +336,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.tetheroffload.config@1.0.vendor \
     android.hardware.tetheroffload.control@1.1.vendor \
-    android.hardware.wifi@1.0-service-lazy
+    android.hardware.wifi-service
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
